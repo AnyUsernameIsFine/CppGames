15743c6c294c8b1b2df76ac2dd10fd4d7cfd496d
AnyUsernameIsFine <anyusernameisfine@protonmail.com>
2018-02-11 00:26:52 +0100

    Use recursion to draw nested coordinate systems

--- SpaceSimulator/CoordinateSystem.cpp
+++ SpaceSimulator/CoordinateSystem.cpp
@@ -10,70 +10,70 @@ namespace Game
 
 	void CoordinateSystem::draw(const Camera& camera) const
 	{
-		int method = 1;
+		// draw universe
+		glm::mat4 p = camera.getProjectionMatrix();
+		glm::mat4 v = camera.getViewMatrix(true);
+
+		// determine the ultimate rotation of the universe as seen through the camera
+		glm::mat4 universeRotate = glm::mat4(1);
+		CoordinateSystem* cameraSystem = (CoordinateSystem*)camera.coordinateSystem;
+		while (cameraSystem) {
+			universeRotate *= glm::inverse(cameraSystem->transform.getRotateMatrix());
+			cameraSystem = (CoordinateSystem*)cameraSystem->parent;
+		}
+
+		// draw the universe
+		glm::mat4 matrix = p * v * universeRotate;
+		draw(matrix, { 0, 1, 0 });
+
+		// save camera matrices
+		p = camera.getProjectionMatrix();
+		v = camera.getViewMatrix();
+		glm::mat4 v2 = camera.getViewMatrix(true);
+
+
+		float ratio;
+		if (camera.coordinateSystem->parent) {
+			ratio = ((CoordinateSystem*)camera.coordinateSystem->parent)->scale / ((CoordinateSystem*)camera.coordinateSystem)->scale;
+		}
+		else {
+			ratio = 1.0f;
+		}
+		glm::mat4 s = glm::scale(glm::mat4(1), { ratio, ratio, ratio });
+
+		v *= s;
+		v2 *= s;
+
+		// determine camera coordinate sytem hierarchy
+		std::vector<glm::mat4> rotations;
+		std::vector<CoordinateSystem*> cameraSystems;
+		cameraSystem = (CoordinateSystem*)(camera.coordinateSystem);
+		bool first = true;
+		while (cameraSystem->parent) {
+			float ratio = 1;
+			if (cameraSystem->parent->parent) {
+				ratio =
+					((CoordinateSystem*)cameraSystem->parent)->scale /
+					((CoordinateSystem*)cameraSystem->parent->parent)->scale;
+			}
+			glm::mat4 rotate = glm::scale(glm::mat4(1), { ratio, ratio, ratio });
+			rotate *= cameraSystem->transform.getModelMatrix();
+			rotations.push_back(glm::inverse(rotate));
+			cameraSystems.push_back(cameraSystem);
+			cameraSystem = (CoordinateSystem*)(cameraSystem->parent);
+		}
+
+		//output("here we go");
+
+		int method = 2;
 		if (method & 1) {
-			// draw universe
-			glm::mat4 p = camera.getProjectionMatrix();
-			glm::mat4 v = camera.getViewMatrix(true);
-
-			// determine the ultimate rotation of the universe as seen through the camera
-			glm::mat4 universeRotate = glm::mat4(1);
-			CoordinateSystem* cameraSystem = (CoordinateSystem*)camera.coordinateSystem;
-			while (cameraSystem) {
-				universeRotate *= glm::inverse(cameraSystem->transform.getRotateMatrix());
-				cameraSystem = (CoordinateSystem*)cameraSystem->parent;
-			}
-
-			// draw the universe
-			glm::mat4 matrix = p * v * universeRotate;
-			draw(matrix, { 0, 1, 0 });
-
-			// save camera matrices
-			p = camera.getProjectionMatrix();
-			v = camera.getViewMatrix();
-			glm::mat4 v2 = camera.getViewMatrix(true);
-
-
-			float ratio;
-			if (camera.coordinateSystem->parent) {
-				ratio = ((CoordinateSystem*)camera.coordinateSystem->parent)->scale / ((CoordinateSystem*)camera.coordinateSystem)->scale;
-			}
-			else {
-				ratio = 1.0f;
-			}
-			glm::mat4 s = glm::scale(glm::mat4(1), { ratio, ratio, ratio });
-
-			v *= s;
-			v2 *= s;
-
-			// determine camera coordinate sytem hierarchy
-			std::vector<glm::mat4> rotations;
-			std::vector<CoordinateSystem*> cameraSystems;
-			cameraSystem = (CoordinateSystem*)(camera.coordinateSystem);
-			bool first = true;
-			while (cameraSystem->parent) {
-				float ratio = 1;
-				if (cameraSystem->parent->parent) {
-					ratio =
-						((CoordinateSystem*)cameraSystem->parent)->scale /
-						((CoordinateSystem*)cameraSystem->parent->parent)->scale;
-				}
-				glm::mat4 rotate = glm::scale(glm::mat4(1), { ratio, ratio, ratio });
-				rotate *= cameraSystem->transform.getModelMatrix();
-				rotations.push_back(glm::inverse(rotate));
-				cameraSystems.push_back(cameraSystem);
-				cameraSystem = (CoordinateSystem*)(cameraSystem->parent);
-			}
-
-			output("here we go");
-
 			// draw galaxies
 			for (auto& galaxy : coordinateSystems) {
 				glm::mat4 matrix;
 
 				glm::mat4 m2 = galaxy.transform.getModelMatrix();
 
-				if (&galaxy == camera.coordinateSystem) {
+				if (!cameraSystems.empty() && cameraSystems.back() == &galaxy) {
 					matrix = p * v;
 				}
 				else {
@@ -82,6 +82,10 @@ namespace Game
 						matrix *= rotation;
 					}
 					matrix *= m2;
+				}
+
+				if (!cameraSystems.empty() && cameraSystems.back() == &galaxy) {
+					cameraSystems.pop_back();
 				}
 
 				glm::mat4 rs = glm::scale(glm::mat4(1), { galaxy.radius, galaxy.radius, galaxy.radius });
@@ -95,7 +99,7 @@ namespace Game
 					glm::mat4 s3 = glm::scale(glm::mat4(1), { r, r, r });
 					glm::mat4 m3 = star.transform.getModelMatrix();
 
-					if (&star == camera.coordinateSystem) {
+					if (!cameraSystems.empty() && cameraSystems.front() == &star) {
 						matrix = p * v;
 					}
 					else {
@@ -103,7 +107,11 @@ namespace Game
 						for (auto rotation : rotations) {
 							matrix *= rotation;
 						}
-						matrix *= m2 * s3 * m3 * rs;
+						matrix *= m2 * s3 * m3;
+					}
+
+					if (!cameraSystems.empty() && cameraSystems.back() == &star) {
+						cameraSystems.pop_back();
 					}
 
 					glm::mat4 rs = glm::scale(glm::mat4(1), { star.radius, star.radius, star.radius });
@@ -117,7 +125,7 @@ namespace Game
 						glm::mat4 s4 = glm::scale(glm::mat4(1), { r, r, r });
 						glm::mat4 m4 = planet.transform.getModelMatrix();
 
-						if (&planet == camera.coordinateSystem) {
+						if (!cameraSystems.empty() && cameraSystems.front() == &planet) {
 							matrix = p * v;
 						}
 						else {
@@ -126,6 +134,10 @@ namespace Game
 								matrix *= rotation;
 							}
 							matrix *= m2 * s3 * m3 * s4 * m4;
+						}
+
+						if (!cameraSystems.empty() && cameraSystems.back() == &planet) {
+							cameraSystems.pop_back();
 						}
 
 						glm::mat4 rs = glm::scale(glm::mat4(1), { planet.radius, planet.radius, planet.radius });
@@ -139,8 +151,13 @@ namespace Game
 							glm::mat4 s5 = glm::scale(glm::mat4(1), { r, r, r });
 							glm::mat4 m5 = moon.transform.getModelMatrix();
 
-							if (&moon == camera.coordinateSystem) {
+							if (!cameraSystems.empty() && cameraSystems.front() == &moon) {
 								matrix = p * v;
+							}
+							else if (moon.parent == camera.coordinateSystem) {
+								matrix = p * v2;
+
+								matrix *= s5 * moon.transform.getModelMatrix(camera.transform.getPosition());
 							}
 							else {
 								matrix = p * v;
@@ -150,6 +167,10 @@ namespace Game
 								matrix *= m2 * s3 * m3 * s4 * m4 * s5 * m5;
 							}
 
+							if (!cameraSystems.empty() && cameraSystems.back() == &moon) {
+								cameraSystems.pop_back();
+							}
+
 							glm::mat4 rs = glm::scale(glm::mat4(1), { moon.radius, moon.radius, moon.radius });
 							moon.draw(matrix * rs, { 1, 0, 1 });
 						}
@@ -157,11 +178,76 @@ namespace Game
 				}
 			}
 		}
+
+		if (method & 2) {
+			// draw galaxies
+			for (auto& galaxy : coordinateSystems) {
+				galaxy.drawRecursively(s, glm::mat4(1), camera, cameraSystems, rotations, 1);
+			}
+		}
 	}
 
-	void CoordinateSystem::draw(const glm::mat4& matrix, glm::vec3 color) const
+	void CoordinateSystem::drawRecursively(const glm::mat4& initialScaleMatrix, glm::mat4 passMatrix, const Camera& camera, std::vector<CoordinateSystem*>& cameraSystems, std::vector<glm::mat4> rotations, int depth) const
 	{
-		shaderProgram_->use();
+		glm::mat4 drawMatrix = camera.getProjectionMatrix() * camera.getViewMatrix() * initialScaleMatrix;
+
+		float r = 1.0f;
+		if (parent->parent) {
+			r = ((CoordinateSystem*)parent)->scale / ((CoordinateSystem*)parent->parent)->scale;
+		}
+		glm::mat4 relativeScaleMatrix = glm::scale(glm::mat4(1), { r, r, r });
+
+		glm::mat4 modelMatrix = transform.getModelMatrix();
+
+		passMatrix *= relativeScaleMatrix * modelMatrix;
+
+		if (!cameraSystems.empty() && cameraSystems.front() == this) {
+			//passMatrix = camera.getProjectionMatrix() * camera.getViewMatrix(true) * initialScaleMatrix;
+		}
+		else {
+			for (auto rotation : rotations) {
+				drawMatrix *= rotation;
+			}
+			drawMatrix *= passMatrix;
+		}
+
+		if (!cameraSystems.empty() && cameraSystems.back() == this) {
+			cameraSystems.pop_back();
+
+			//rotations.pop_back();
+		}
+
+		drawMatrix *= glm::scale(glm::mat4(1), { radius, radius, radius });
+		draw(drawMatrix, depth);
+
+		for (auto& coordinateSystem : coordinateSystems) {
+			coordinateSystem.drawRecursively(initialScaleMatrix, passMatrix, camera, cameraSystems, rotations, depth + 1);
+		}
+	}
+
+	void CoordinateSystem::draw(const glm::mat4& matrix, int depth) const
+	{
+		glm::vec3 color;
+		switch (depth) {
+		case 1:
+			color = { 0, 0, 1 };
+			break;
+		case 2:
+			color = { 1, 0, 0 };
+			break;
+		case 3:
+			color = { 1, 1, 0 };
+			break;
+		case 4:
+			color = { 1, 0, 1 };
+			break;
+		}
+		draw(matrix, color);
+	}
+
+		void CoordinateSystem::draw(const glm::mat4& matrix, glm::vec3 color) const
+		{
+			shaderProgram_->use();
 		shaderProgram_->setUniform("color", glm::vec4(color, 1));
 		shaderProgram_->setUniform("matrix", matrix);
 
--- SpaceSimulator/CoordinateSystem.h
+++ SpaceSimulator/CoordinateSystem.h
@@ -23,6 +23,16 @@ namespace Game
 		void draw(const Camera& camera) const;
 		void draw(const glm::mat4& matrix, glm::vec3 color) const;
 
+		void drawRecursively(
+			const glm::mat4& initialScaleMatrix,
+			glm::mat4 passMatrix,
+			const Camera& camera,
+			std::vector<CoordinateSystem*>& cameraSystems,
+			std::vector<glm::mat4> rotations,
+			int depth
+		) const;
+		void draw(const glm::mat4& matrix, int depth) const;
+
 	protected:
 		static GLuint vao_;
 		static ShaderProgram* shaderProgram_;
