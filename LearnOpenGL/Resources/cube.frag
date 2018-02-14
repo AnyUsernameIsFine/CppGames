489ab925074dd97bc02bcb390d8808372b9a93cb
AnyUsernameIsFine <anyusernameisfine@protonmail.com>
2018-02-14 23:50:19 +0100

    Advance LearnOpenGL tutorials to Lighting / Colors

--- LearnOpenGL/Resources/cube.frag
+++ LearnOpenGL/Resources/cube.frag
@@ -7,8 +7,9 @@ out vec4 fragColor;
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 uniform float mix;
+uniform vec4 lightColor;
 
 void main()
 {
-    fragColor = mix(texture(texture1, vertTexCoords), texture(texture2, vertTexCoords), mix);
+    fragColor = lightColor * mix(texture(texture1, vertTexCoords), texture(texture2, vertTexCoords), mix);
 }
--- LearnOpenGL/Source/Main.h
+++ LearnOpenGL/Source/Main.h
@@ -9,12 +9,16 @@ namespace Game
 	class Game : public Framework::Game
 	{
 	public:
-		ShaderProgram * program;
+		Camera<> camera;
+
+		ShaderProgram* cubeShader;
+		ShaderProgram* lightShader;
 		Texture2D* texture1;
 		Texture2D* texture2;
-		GLuint VAO;
-		Camera<> camera;
+		GLuint cubeVao;
+		GLuint lightVao;
 		std::vector<Transform<>> cubes;
+		Transform<> light;
 
 		Game()
 		{
@@ -26,37 +30,47 @@ namespace Game
 
 		void start()
 		{
-			for (int i = 0; i < 10; i++) {
-				float x = 6 * (float)rand() / RAND_MAX - 3;
-				float y = 6 * (float)rand() / RAND_MAX - 3;
-				float z = 6 * (float)rand() / RAND_MAX - 3;
-				cubes.push_back(Transform<>({ x, y, z }));
-			}
-
 			graphics.text.setFont("Resources/consola.ttf", 16);
 			graphics.text.setColor(1, 1, 1);
 
 			camera.setAspectRatio((float)graphics.window.getWidth() / graphics.window.getHeight());
 			camera.transform.moveZ(6);
 
+			for (int i = 0; i < 10; i++) {
+				float x = 6 * (float)rand() / RAND_MAX - 3;
+				float y = 6 * (float)rand() / RAND_MAX - 3;
+				float z = 6 * (float)rand() / RAND_MAX - 3;
+
+				Transform<> cube({ x, y, z });
+				cube.scale({ 2, 1, 0.5f });
+				cube.rotate(i * 20.0f, { -1.0f, 0.3f, 0.5f });
+
+				cubes.push_back(cube);
+			}
+
+			light.setPosition(1.2f, 1.0f, 2.0f);
+			light.scale(0.2f);
+
 			texture1 = new Texture2D("Resources/journey.jpg");
 			texture2 = new Texture2D("Resources/flow.jpg");
 
-			program = new ShaderProgram("Resources/simple.vert", "Resources/simple.frag");
+			cubeShader = new ShaderProgram("Resources/cube.vert", "Resources/cube.frag");
 
-			program->use();
-			program->setUniform("texture1", 0);
-			program->setUniform("texture2", 1);
+			cubeShader->use();
+			cubeShader->setUniform("texture1", 0);
+			cubeShader->setUniform("texture2", 1);
+
+			lightShader = new ShaderProgram("Resources/light.vert", "Resources/light.frag");
 
 			float vertices[] = {
 				-0.5, -0.5, -0.5,	0, 0,
-				0.5, -0.5, -0.5,	1, 0,
+				 0.5, -0.5, -0.5,	1, 0,
 				-0.5,  0.5, -0.5,	0, 1,
-				0.5,  0.5, -0.5,	1, 1,
+				 0.5,  0.5, -0.5,	1, 1,
 				-0.5, -0.5,  0.5,	0, 0,
-				0.5, -0.5,  0.5,	1, 0,
+				 0.5, -0.5,  0.5,	1, 0,
 				-0.5,  0.5,  0.5,	0, 1,
-				0.5,  0.5,  0.5,	1, 1,
+				 0.5,  0.5,  0.5,	1, 1,
 			};
 
 			int indices[] = {
@@ -68,16 +82,18 @@ namespace Game
 				4, 5, 6, 6, 5, 7,
 			};
 
-			for (int i = 0; i < (int)cubes.size(); i++) {
-				cubes[i].scale({ 2, 1, 0.5f });
-				cubes[i].rotate(i * 20.0f, { -1.0f, 0.3f, 0.5f });
-
-			}
-
-			glGenVertexArrays(1, &VAO);
-			glBindVertexArray(VAO);
+			glGenVertexArrays(1, &cubeVao);
+			glBindVertexArray(cubeVao);
 			VertexBufferObject vbo({ 3, 2 }, 8, vertices);
 			IndexBufferObject ibo(36, indices);
+			glBindVertexArray(0);
+
+			glGenVertexArrays(1, &lightVao);
+			glBindVertexArray(lightVao);
+			glBindBuffer(GL_ARRAY_BUFFER, vbo.getId());
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), nullptr);
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo.getId());
+			glEnableVertexAttribArray(0);
 			glBindVertexArray(0);
 		}
 
@@ -114,15 +130,16 @@ namespace Game
 
 		void draw()
 		{
-			graphics.clearScreen(1, 0, 0.5);
+			graphics.clearScreen(0, 0, 0);
 
 			glm::mat4 view = camera.getViewMatrix();
 			glm::mat4 projection = camera.getProjectionMatrix();
 
-			program->use();
-			program->setUniform("view", view);
-			program->setUniform("projection", projection);
-			program->setUniform("mix", (sin(graphics.getTotalSeconds()) + 1) * 0.5f);
+			cubeShader->use();
+			cubeShader->setUniform("view", view);
+			cubeShader->setUniform("projection", projection);
+			cubeShader->setUniform("mix", (sin(graphics.getTotalSeconds()) + 1) * 0.5f);
+			cubeShader->setUniform("lightColor", glm::vec4(1, 1, 1, 1));
 
 			texture1->use(0);
 			texture2->use(1);
@@ -130,17 +147,22 @@ namespace Game
 			glEnable(GL_DEPTH_TEST);
 			glEnable(GL_CULL_FACE);
 
-			glBindVertexArray(VAO);
+			glBindVertexArray(cubeVao);
 
 			for (int i = 0; i < (int)cubes.size(); i++) {
 				cubes[i].rotate(graphics.getDeltaSeconds() * 50.0f, { 1.0f, 0.3f, 0.5f });
-
 				glm::mat4 model = cubes[i].getModelMatrix();
-
-				program->setUniform("model", model);
-
+				cubeShader->setUniform("model", model);
 				glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, nullptr);
 			}
+
+			lightShader->use();
+			glm::mat4 model = light.getModelMatrix();
+			lightShader->setUniform("model", model);
+			lightShader->setUniform("view", view);
+			lightShader->setUniform("projection", projection);
+			glBindVertexArray(lightVao);
+			glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, nullptr);
 
 			int fps = (int)round(graphics.getFps());
 			Vector3<> position = camera.transform.getPosition();
